215de0fc50976ca45ffb2d161c2c7b2429dbcc90
stachedata <stachedata@gmail.com>
2019-03-28 16:15:06 -0400

    create array with slider values

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-document.getElementById("play").onclick = () => document.getElementById("tada").play()  
+// document.getElementById("play").onclick = () => document.getElementById("tada").play()  
+const sliders = [...document.getElementsByTagName('input')]
+
+const printArray = (sliders) => {
+    const slidersValues = []
+    sliders.map(x => {
+        slidersValues.push(x.value)
+    })
+    return slidersValues
+}
+
+document.getElementById('printArray').onclick = () => console.log(printArray(sliders))
