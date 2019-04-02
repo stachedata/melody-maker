63627ecfb41a2d8e97412f0e614ef3bcea29ac5d
Chris Anastasi <stachedata@gmail.com>
2019-04-02 19:25:15 -0400

    dynamically display chosen notes

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const playMelody = (melodyNotes, scale) => {
     }
 
     let i = 0
-    const tempo = 400
+    const tempo = 300
 
     const melody = setInterval(() => { 
         createOscillator(scale[melodyNotes[i]],holdNote(melodyNotes,i))
@@ -51,3 +51,54 @@ const createOscillator = (frequency, hold) => {
 }
 
 document.getElementById('play').onclick = () => playMelody(melodyNotes(noteValues),scale)
+
+const displayNotes = (notes) => {
+    let noteDiv = document.getElementsByClassName("noteDiv")
+    console.log(noteDiv)
+    notes.forEach( (note,i) => { 
+        const updateNotes = () => {
+            switch(note.value){
+                case '0':
+                    noteDiv[i].innerText = "Zz"
+                    break
+                case '1':
+                    noteDiv[i].innerText = "-"  
+                    break
+                case '2':
+                case '9':
+                    noteDiv[i].innerText = "G"
+                    break
+                case '3':
+                case '10':
+                    noteDiv[i].innerText = "A"
+                    break
+                case '4':
+                case '11':
+                    noteDiv[i].innerText = "B"
+                    break
+                case '5':
+                case '12':
+                    noteDiv[i].innerText = "C"  
+                    break
+                case '6':
+                case '13':
+                    noteDiv[i].innerText = "D"
+                    break
+                case '7':
+                case '14':
+                    noteDiv[i].innerText = "E"
+                    break
+                case '8':
+                    noteDiv[i].innerText = "F"
+                    break
+                case '15':
+                    noteDiv[i].innerText = "?"
+                    break
+            }
+
+        }
+        updateNotes()
+        note.oninput = () => updateNotes()
+    })
+} 
+displayNotes(noteValues)
